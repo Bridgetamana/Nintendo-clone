44cbcbbb5776431609b73fc0028a85fe0ed05e29
Bridget Amana <amanabridget03@gmail.com>
2024-06-25 23:04:07 +0100

    install react icon

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav>
+        <div>
+            <div>
+                <img src="" alt="Nintendo Logo" />
+            </div>
+            <div>
+                <a href="#">My Nintendo Store</a>
+                <span></span>
+            </div>
+        </div>
+        <div>
+            <span>
+                <p>Free shipping</p>on orders $50 or more. <a href="#">Restrictions apply</a>
+            </span>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
---
+++ src/components/Product/Product.jsx
@@ -0,0 +1,94 @@
+import React from 'react'
+import "./Product.css";
+
+function Product() {
+  return (
+    <main>
+      <div>
+        <section>
+          <div>
+            <div>
+              <ol>
+                <li>
+                  <a href="#">Store</a>
+                  svg
+                </li>
+                <li>
+                  <a href="#">Games</a>
+                  svg
+                </li>
+                <li>
+                  <a href="#">Stardew Valley</a>
+                </li>
+              </ol>
+            </div>
+            <div>
+              <div>
+                <img src="" alt="Stardew Valley" />
+              </div>
+              <div>
+                <img src="" alt="Stardew Valley" />
+                <img src="" alt="Stardew Valley" />
+                <img src="" alt="Stardew Valley" />
+                <img src="" alt="Stardew Valley" />
+                <img src="" alt="Stardew Valley" />
+                <img src="" alt="Stardew Valley" />
+                <img src="" alt="Stardew Valley" />
+              </div>
+            </div>
+          </div>
+          <div>
+            <span>
+              <p>Nintendo Switch</p>
+            </span>
+            <div>
+              <h2>Stardew Valley</h2>
+              <div>
+                <div>
+                  <h2></h2>
+                  <p></p>
+                </div>
+                <div></div>
+              </div>
+              <button>
+                <p>s</p>
+                <p>s</p>
+                <p>s</p>
+              </button>
+              <p>
+                This item will be sent to your system automatically after
+                purchase.
+              </p>
+            </div>
+          </div>
+        </section>
+        <section>
+          <div>
+            <h3>You&apos;re moving to the valley</h3>
+            <p>
+              You’ve inherited your grandfather’s old farm plot in Stardew
+              Valley. Armed with hand-me-down tools and a few coins, you set out
+              to begin your new life. Can you learn to live off the land and
+              turn these overgrown fields into a thriving home? It won’t be
+              easy. Ever since Joja Corporation came to town, the old ways of
+              life have all but disappeared. The community center, once the
+              town’s most vibrant hub of activity, now lies in shambles. But the
+              valley seems full of opportunity. With a little dedication, you
+              might just be the one to restore Stardew Valley to greatness!
+            </p>
+            <button>
+              + <a href="#">Read more</a>
+            </button>
+            <p>Software description provided by the publisher.</p>
+            <button>Explore this game&apos;s official website</button>
+          </div>
+          <div>
+            <img src="" alt="Stardew Valley" />
+          </div>
+        </section>
+      </div>
+    </main>
+  );
+}
+
+export default Product

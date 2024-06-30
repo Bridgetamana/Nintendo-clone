70d10c57ac79ad85b21aefd83790e58f90ced3f1
Bridget Amana <amanabridget03@gmail.com>
2024-06-30 03:48:24 +0100

    update footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,154 +1,224 @@
-import React from 'react'
+import React from "react";
+import Logo from "../../assets/img/nintendo-logo.jpeg";
+import Flag from "../../assets/img/united-states-flag.png";
+import "./Footer.css";
+import { FaFacebook, FaInstagram } from "react-icons/fa";
+import { FaYoutube } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+import Certificate from "../../assets/img/esrb-certificate.png";
 
 const Footer = () => {
   return (
     <footer>
       <div>
-        <div className="flex-center">
+        <div className="flex-center social-links-wrapper">
           <div>
-            <img src="" alt="Nintendo Logo" />
+            <img src={Logo} alt="Nintendo Logo" />
           </div>
-          <div className="flex gap-12">
-            <p>F</p>
-            <p>I</p>
-            <p>X</p>
-            <p>Y</p>
+          <div className="flex gap-12 social-links">
+            <a href="" target="_blank" rel="noopener noreferrer">
+              <FaFacebook fill="#fff" size={30} />
+            </a>
+            <a href="" target="_blank" rel="noopener noreferrer">
+              <FaInstagram fill="#fff" size={30} />
+            </a>
+            <a href="" target="_blank" rel="noopener noreferrer">
+              <FaXTwitter fill="#fff" size={30} />
+            </a>
+            <a href="" target="_blank" rel="noopener noreferrer">
+              <FaYoutube fill="#fff" size={30} />
+            </a>
           </div>
         </div>
-        <div className="">
-          <div>
-            <p>About Nintendo</p>
+        <div className="footer-links-wrapper">
+          <div className="footer-links">
+            <h4>About Nintendo</h4>
             <ul>
               <li>
-                <a href="#">Careers</a>
+                <a href="#" className="links">
+                  Careers
+                </a>
               </li>
               <li>
-                <a href="#">Corporate Social Resposibility</a>
+                <a href="#" className="links">
+                  Corporate Social Resposibility
+                </a>
               </li>
             </ul>
           </div>
-          <div>
-            <p>Shop</p>
+          <div className="footer-links">
+            <h4>Shop</h4>
             <ul>
               <li>
-                <a href="#">Games</a>
+                <a href="#" className="links">
+                  Games
+                </a>
               </li>
               <li>
-                <a href="#">Hardware</a>
+                <a href="#" className="links">
+                  Hardware
+                </a>
               </li>
               <li>
-                <a href="#">Merchandise</a>
+                <a href="#" className="links">
+                  Merchandise
+                </a>
               </li>
               <li>
-                <a href="#">Sales & deals</a>
+                <a href="#" className="links">
+                  Sales & deals
+                </a>
               </li>
               <li>
-                <a href="#">Exclusives</a>
+                <a href="#" className="links">
+                  Exclusives
+                </a>
               </li>
               <li>
-                <a href="#">Online service</a>
+                <a href="#" className="links">
+                  Online service
+                </a>
               </li>
               <li>
-                <a href="#">Nintendo NY store</a>
+                <a href="#" className="links">
+                  Nintendo NY store
+                </a>
               </li>
             </ul>
           </div>
-          <div>
-            <p>My Nintendo Store orders</p>
+          <div className="footer-links">
+            <h4>My Nintendo Store orders</h4>
             <ul>
               <li>
-                <a href="">Order details</a>
+                <a href="" className="links">
+                  Order details
+                </a>
               </li>
               <li>
-                <a href="#">Shipping info</a>
+                <a href="#" className="links">
+                  Shipping info
+                </a>
               </li>
               <li>
-                <a href="#">Returns & exchanges</a>
+                <a href="#" className="links">
+                  Returns & exchanges
+                </a>
               </li>
               <li>
-                <a href="#">FAQ</a>
+                <a href="#" className="links">
+                  FAQ
+                </a>
               </li>
             </ul>
           </div>
-          <div>
-            <p>Support</p>
+          <div className="footer-links">
+            <h4>Support</h4>
             <ul>
               <li>
-                <a href="#">Nintendo Switch</a>
+                <a href="#" className="links">
+                  Nintendo Switch
+                </a>
               </li>
               <li>
-                <a href="#">Nintendo Account</a>
+                <a href="#" className="links">
+                  Nintendo Account
+                </a>
               </li>
               <li>
-                <a href="#">Other systems</a>
+                <a href="#" className="links">
+                  Other systems
+                </a>
               </li>
               <li>
-                <a href="#">Repairs</a>
+                <a href="#" className="links">
+                  Repairs
+                </a>
               </li>
               <li>
-                <a href="#">Nintendo product recycling</a>
+                <a href="#" className="links">
+                  Nintendo product recycling
+                </a>
               </li>
             </ul>
           </div>
-          <div>
-            <p>Parents</p>
+          <div className="footer-links">
+            <h4>Parents</h4>
             <ul>
               <li>
-                <a href="#">Info for parents</a>
+                <a href="#" className="links">
+                  Info for parents
+                </a>
               </li>
               <li>
-                <a href="#">Parental controls</a>
+                <a href="#" className="links">
+                  Parental controls
+                </a>
               </li>
             </ul>
           </div>
-          <div>
-            <p>Community</p>
+          <div className="footer-links">
+            <h4>Community</h4>
             <ul>
               <li>
-                <a href="#">Community guidelines</a>
+                <a href="#" className="links">
+                  Community guidelines
+                </a>
               </li>
               <li>
-                <a href="#">Online safety principles</a>
+                <a href="#" className="links">
+                  Online safety principles
+                </a>
               </li>
             </ul>
           </div>
-          <div>
-            <p>Privacy</p>
+          <div className="footer-links">
+            <h4>Privacy</h4>
             <ul>
               <li>
-                <a href="#">Privacy policy</a>
+                <a href="#" className="links">
+                  Privacy policy
+                </a>
               </li>
               <li>
-                <a href="#">Cookies and interest-based ads</a>
+                <a href="#" className="links">
+                  Cookies and interest-based ads
+                </a>
               </li>
             </ul>
-          </div>
-          <div>
-            <img src="" alt="" />
           </div>
         </div>
-        <div>
+        <div className="esrb-certificate">
+          <img src={Certificate} alt="" />
+        </div>
+        <div className="policy">
           <p>
             © Nintendo. Games are property of their respective owners. Nintendo
             of America Inc. Headquarters are in Redmond, Washington, USA
           </p>
-          <div>
-            <ul>
-              <li>
-                <a href="#">Contact us</a>
-                <a href="#">Website feedback</a>
-                <a href="#">Terms of Use</a>
-                <a href="#">Documents & Policies</a>
-              </li>
-            </ul>
-          </div>
-          <div>
+          <ul className="policy-links">
+            <li>
+              <a href="#">Contact us</a>
+            </li>
+            <li>
+              <a href="#">Website feedback</a>
+            </li>
+            <li>
+              <a href="#">Terms of Use</a>
+            </li>
+            <li>
+              <a href="#">Documents & Policies</a>
+            </li>
+          </ul>
+          <div className="language">
+            <div>
+              <img src={Flag} alt="Flag of USA" />
+            </div>
             <a href="#">English (United States)</a>
           </div>
         </div>
       </div>
     </footer>
   );
-}
+};
 
-export default Footer
+export default Footer;
--- src/components/More-like-this/MoreLikeThis.jsx
+++ src/components/More-like-this/MoreLikeThis.jsx
@@ -1,21 +1,20 @@
-import React from 'react'
+import React from "react";
+import "./MoreLikeThis.css";
+import Portia from "../../assets/img/portia.avif";
 
 const MoreLikeThis = () => {
-
-  
-  
   return (
     <section>
       <div>
-        <div className='flex-center'>
-          <button>Online</button>
-          <p>
+        <div className="access-card">
+          <button className="online-btn">Online</button>
+          <h3>
             Play online, access classic NES™ and Super NES™ games, and more with
             a Nintendo Switch Online membership.
-          </p>
-          <span className='flex'>
+          </h3>
+          <span className="flex">
             <p>This game supports:</p>
-            <ul className='flex'>
+            <ul className="flex">
               <li>
                 <a href="#">Online Play</a>
               </li>
@@ -24,32 +23,45 @@ const MoreLikeThis = () => {
               </li>
             </ul>
           </span>
-          <button>Learn more</button>
+          <button className="learn-more-btn">Learn more</button>
         </div>
         <div>
           <h2>More like this</h2>
-          <div className='flex gap-12'>
-            <div>
-              <div>
-                <img src="" alt="" />
+          <div>
+            <div className="more-game-card">
+              <div className="more-game-img">
+                <img src={Portia} alt="" />
               </div>
-              <div>
-                <span>
-                  <h3>My Time at Portia</h3>
+              <div className="more-game-text">
+                <span className="title">
+                  <h4>My Time at Portia</h4>
                   <p>4/16/19</p>
                 </span>
-                <span>
-                  <p>$29.99</p>
-                  <span className='space-between'>
+                <p className="price">$29.99</p>
+                <span className="wishlist-btn">
+                  <span className="flex">
+                    <div className="red-border"></div>
                     <p>Nintendo Switch</p>
-                    <p>Love</p>
                   </span>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    width={24}
+                    viewBox="0 0 24 24"
+                    stroke="rgb(230, 0, 18)"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z"
+                    />
+                  </svg>
                 </span>
               </div>
             </div>
           </div>
         </div>
-        <div>
+        <div className="warning">
           <p>
             WARNING: If you have epilepsy or have had seizures or other unusual
             reactions to flashing lights or patterns, consult a doctor before
@@ -57,19 +69,21 @@ const MoreLikeThis = () => {
             Information available in the system settings before using this
             software.
           </p>
-          <p>
+          <br />
+          <p className="small-text">
             Nintendo Switch Online membership (sold separately) and Nintendo
             Account required for online play. Not available in all countries.
             Internet access required for online features. Terms apply.
             nintendo.com/switch-online
           </p>
-          <p>
+          <br />
+          <p className="small-text">
             Stardew Valley © ConcernedApe LLC 2016-2020. All rights reserved.
           </p>
         </div>
       </div>
     </section>
   );
-}
+};
 
-export default MoreLikeThis
+export default MoreLikeThis;
